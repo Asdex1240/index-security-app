a04cb3feb3929d93a5e9066b14d6369eb32ded61
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2025-08-16 10:08:09 -0600

    feat(sidebar): Add sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'login',
+        redirectTo: 'dashboard',
         pathMatch: 'full'
     },
     {
         path: 'login',
         loadComponent: () => import('./pages/auth/login/login').then(m => m.Login)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/features/dashboard/dashboard').then(m => m.Dashboard)
     }
 ];
